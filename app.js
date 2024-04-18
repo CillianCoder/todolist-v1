5f6f71c6c6755db52e110f8b2dec6823204fe41e
CillianCoder <msandakelum838@gmail.com>
2024-04-19 01:37:42 +0530

    declare variables

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-var item = ['Buy Food', 'Cook Food', 'Eat Food'];
+let item = ['Buy Food', 'Cook Food', 'Eat Food'];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,22 +11,22 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function (req, res) {
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-Us", options);
+    let day = today.toLocaleDateString("en-Us", options);
 
     res.render("list", { kindOfDay: day, newListItems: item});
 
 });
 
 app.post("/", function(req, res){
-    var items = req.body.addItem;
+    let items = req.body.addItem;
     item.push(items);
     res.redirect("/");
 })
